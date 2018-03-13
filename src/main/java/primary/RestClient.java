e48a6c8377ef1cf22464183d876d19259dec5ccd
Timothy <timothy.carlisle@d-fine.co.uk>
2018-03-13 13:05:16 +0000

    constructor added and connect with parameters

--- src/main/java/primary/RestClient.java
+++ src/main/java/primary/RestClient.java
@@ -1,25 +1,67 @@
 package primary;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Base64;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.json.Json;
+import javax.json.JsonObject;
 
 public class RestClient {
 
-    public final  String username = "admin";
-    public final String password = "admin";
+    public final  String username = "demo";
+    public final String password = "demo";
+    public  String camundaUrl ;
 
-    private HttpURLConnection connect(URL url) throws IOException {
+    private HttpURLConnection connect(String parameters) throws  IOException{
+
+        URL url = new URL(camundaUrl + "/" + parameters);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-       // connection.setRequestMethod("GET");
-
-    return connection;
+        String userpass = username + ":" + password;
+        String basicAuth = "Basic " + new String(new Base64().encode(userpass.getBytes()));
+        connection.setRequestProperty ("Authorization", basicAuth);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        return connection;
     }
 
+    private HttpURLConnection connect() throws IOException {
 
+        return connect("");
+    }
+
+    public RestClient(String url) throws IOException{
+        camundaUrl = url;
+       // connect();
+    }
+
+        public void jiraTicketTransition(String issueType, String issueKey, String newStatus) throws IOException {
+        HttpURLConnection connection = connect("process-definition/ProcessMessage/start");
+        connection.setRequestMethod("POST");
+
+        JsonObject jsonBody = Json.createObjectBuilder()
+                .add("variables",
+                        Json.createObjectBuilder().add("issueKey",issueKey)
+                                .add("newStatus", newStatus)
+                                .add("issueType", issueType)
+                ).build();
+
+        String encodedData = jsonBody.toString();
+        connection.getOutputStream().write(encodedData.getBytes());
+
+        try {
+            InputStream inputStream = connection.getInputStream();
+            System.out.println(inputStream);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 
     public RestClient(URL url)  throws IOException {
-        HttpURLConnection connection = connect(url);
+        //do nothing
     }
 }
--- src/main/java/tests/Go.java
+++ src/main/java/tests/Go.java
@@ -1,16 +1,18 @@
+package tests;
+
 import primary.RestClient;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 
 public class Go {
 
     public static void main(String args[]) throws IOException {
-        URL url = new URL("http://localhost:9080/rest/engine");
+        String url = "http://localhost:9080/rest/engine";
 
     RestClient restClient = new RestClient(url);
+    restClient.jiraTicketTransition("EXT-8", "New", "Asked");
 
     int x=1;
 
